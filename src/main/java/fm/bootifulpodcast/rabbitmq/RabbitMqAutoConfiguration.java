8c52b4b205fc11daa60927fd3f25127771d9fa32
Josh Long <josh@joshlong.com>
2020-01-14 17:46:29 -0800

    initial commit.

--- src/main/java/fm/bootifulpodcast/rabbitmq/RabbitMqAutoConfiguration.java
+++ src/main/java/fm/bootifulpodcast/rabbitmq/RabbitMqAutoConfiguration.java
@@ -1,21 +1,23 @@
 package fm.bootifulpodcast.rabbitmq;
 
 import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(prefix = "rabbitmq.utils", name = "enabled", havingValue = "true")
 class RabbitMqAutoConfiguration {
 
-  @Bean
-  RabbitMqHelper rabbitMqHelper(AmqpAdmin amqpAdmin) {
-    return new RabbitMqHelper(amqpAdmin);
-  }
+	@Bean
+	@ConditionalOnMissingBean
+	RabbitMqHelper rabbitMqHelper(AmqpAdmin amqpAdmin) {
+		return new RabbitMqHelper(amqpAdmin);
+	}
 
-  @Bean
-  RabbitMqEnvironmentPostProcessor rabbitMqEnvironmentPostProcessor() {
-    return new RabbitMqEnvironmentPostProcessor();
-  }
+	@Bean
+	@ConditionalOnMissingBean
+	RabbitMqEnvironmentPostProcessor rabbitMqEnvironmentPostProcessor() {
+		return new RabbitMqEnvironmentPostProcessor();
+	}
+
 }
--- src/main/java/fm/bootifulpodcast/rabbitmq/RabbitMqEnvironmentPostProcessor.java
+++ src/main/java/fm/bootifulpodcast/rabbitmq/RabbitMqEnvironmentPostProcessor.java
@@ -13,76 +13,77 @@ import java.util.HashMap;
 @Log4j2
 public class RabbitMqEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
-  @Override
-  public void postProcessEnvironment(ConfigurableEnvironment env,
-                                     SpringApplication application) {
+	@Override
+	public void postProcessEnvironment(ConfigurableEnvironment env,
+			SpringApplication application) {
 
-    var configurationKey = "rmq.address";
-    var envVariable = "RMQ_ADDRESS";
-    var resolvedEnvironmentProperty = System.getenv(envVariable);
-    var resolvedConfigurationProperty = env.getProperty(configurationKey);
-    var rmqAddress = StringUtils.hasText(resolvedEnvironmentProperty)
-        ? resolvedEnvironmentProperty
-        : (StringUtils.hasText(resolvedConfigurationProperty)
-        ? resolvedConfigurationProperty : null);
+		var configurationKey = "rmq.address";
+		var envVariable = "RMQ_ADDRESS";
+		var resolvedEnvironmentProperty = System.getenv(envVariable);
+		var resolvedConfigurationProperty = env.getProperty(configurationKey);
+		var rmqAddress = StringUtils.hasText(resolvedEnvironmentProperty)
+				? resolvedEnvironmentProperty
+				: (StringUtils.hasText(resolvedConfigurationProperty)
+						? resolvedConfigurationProperty : null);
 
-    log.debug("RMQ_ADDRESS: " + rmqAddress + ". Detected RMQ_ADDRESS environment variable");
+		log.debug("RMQ_ADDRESS: " + rmqAddress
+				+ ". Detected RMQ_ADDRESS environment variable");
 
-    if (!StringUtils.hasText(rmqAddress)) {
-      return;
-    }
-    var uri = URI.create(rmqAddress);
-    var userInfo = uri.getUserInfo();
-    var vhost = uri.getPath();
-    var host = uri.getHost();
-    var port = uri.getPort();
-    var user = (String) null;
-    var pw = (String) null;
-    if (StringUtils.hasText(userInfo) && userInfo.contains(":")) {
-      var parts = userInfo.split(":");
-      user = parts[0];
-      pw = parts[1];
-    }
+		if (!StringUtils.hasText(rmqAddress)) {
+			return;
+		}
+		var uri = URI.create(rmqAddress);
+		var userInfo = uri.getUserInfo();
+		var vhost = uri.getPath();
+		var host = uri.getHost();
+		var port = uri.getPort();
+		var user = (String) null;
+		var pw = (String) null;
+		if (StringUtils.hasText(userInfo) && userInfo.contains(":")) {
+			var parts = userInfo.split(":");
+			user = parts[0];
+			pw = parts[1];
+		}
 
-    var map = new HashMap<String, Object>();
-    map.put("host", host);
+		var map = new HashMap<String, Object>();
+		map.put("host", host);
 
-    if (port != -1) {
-      map.put("port", port);
-    }
+		if (port != -1) {
+			map.put("port", port);
+		}
 
-    if (StringUtils.hasText(vhost)) {
-      vhost = vhost.trim();
-      if (!vhost.equalsIgnoreCase("/")) {
-        if (vhost.startsWith("/")) {
-          vhost = vhost.substring(1);
-        }
-        map.put("virtual-host", vhost);
-      }
-    }
+		if (StringUtils.hasText(vhost)) {
+			vhost = vhost.trim();
+			if (!vhost.equalsIgnoreCase("/")) {
+				if (vhost.startsWith("/")) {
+					vhost = vhost.substring(1);
+				}
+				map.put("virtual-host", vhost);
+			}
+		}
 
-    if (StringUtils.hasText(pw)) {
-      map.put("password", pw);
-    }
+		if (StringUtils.hasText(pw)) {
+			map.put("password", pw);
+		}
 
-    if (StringUtils.hasText(user)) {
-      map.put("username", user);
-    }
+		if (StringUtils.hasText(user)) {
+			map.put("username", user);
+		}
 
-    var newMap = new HashMap<String, Object>();
-    map.forEach((p, k) -> newMap.put("spring.fm.bootifulpodcast.rabbitmq." + p, k));
+		var newMap = new HashMap<String, Object>();
+		map.forEach((p, k) -> newMap.put("spring.fm.bootifulpodcast.rabbitmq." + p, k));
 
-    var propertySource = new PropertySource<String>("rmq-environment") {
+		var propertySource = new PropertySource<String>("rmq-environment") {
 
-      @Override
-      public Object getProperty(String name) {
-        if (newMap.containsKey(name))
-          return newMap.get(name);
-        return null;
-      }
-    };
+			@Override
+			public Object getProperty(String name) {
+				if (newMap.containsKey(name))
+					return newMap.get(name);
+				return null;
+			}
+		};
 
-    env.getPropertySources().addLast(propertySource);
-  }
+		env.getPropertySources().addLast(propertySource);
+	}
 
 }
